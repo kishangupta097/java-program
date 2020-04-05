299c25abd157a3c0b1e8644ed14057d347fbfe4f
kishangupta097 <49547121+kishangupta097@users.noreply.github.com>
2020-04-05 16:30:04 +0530

    new Java Program for addition of two number

---
+++ addition.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class add
+{
+  public static void main(String args[])
+  {
+  Scanner sc = new Scanner(System.in);
+  int a=sc.nextInt();
+  int b = sc.nextInt();
+  
+  System.out.println("Addition ="+(a+b));
+  }
+}
